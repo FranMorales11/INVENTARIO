1eeac826d591ff6907bf34ad1cd46cd66a96afa4
FRAN MORALES <moralesfran303@gmail.com>
2025-04-02 23:01:19 -0500

    enrutador y controladores

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 /* importamos el framerwork express*/
 import express from "express"
+import categoriaRoutes from "./routes/categorias.routes.js"
 
 /* asignamos a app toda funcionalidad para mi server web*/
 const app =express();
@@ -7,5 +8,8 @@ const app =express();
 /* setear un puerto a mi web server*/
 app.set("port",5000)
 
+/*rutas*/
+app.use("/api/categorias",categoriaRoutes)
+
 /*  hacemos disponible a mi server app para toda la aplicaicon*/
-export default app;
+export default app ;
---
+++ backend/src/routes/categorias.routes.js
@@ -0,0 +1,10 @@
+import {Router} from "express";
+import { methodHTTP as categoriaController } from "../controllers/categoria.controllers.js";
+/* creamos el enrutador*/
+const router = Router();
+
+/* configuramos respuesta desde server metodo http get*/
+router.get("/",categoriaController.getCategorias)
+
+/* hacemos disponible el router en toda la app*/
+export default router;

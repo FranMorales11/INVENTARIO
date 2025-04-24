1270061e20a6e6221aabba01392b68bad439fdb1
FRAN MORALES <moralesfran303@gmail.com>
2025-04-23 22:47:06 -0500

    GET-GET BY ID-DELETE-UPDATE-POST

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 /* importamos el framerwork express*/
 import express from "express"
+import cors from "cors"
 import categoriaRoutes from "./routes/categorias.routes.js"
 
 /* asignamos a app toda funcionalidad para mi server web*/
@@ -8,8 +9,14 @@ const app =express();
 /* setear un puerto a mi web server*/
 app.set("port",5000)
 
+/*Middleware*/
+app.use(express.json());
+
 /*rutas*/
 app.use("/api/categorias",categoriaRoutes)
+
+/*CORS*/
+app.use(cors());
 
 /*  hacemos disponible a mi server app para toda la aplicaicon*/
 export default app ;
--- backend/src/routes/categorias.routes.js
+++ backend/src/routes/categorias.routes.js
@@ -4,7 +4,20 @@ import { methodHTTP as categoriaController } from "../controllers/categoria.cont
 const router = Router();
 
 /* configuramos respuesta desde server metodo http get*/
-router.get("/",categoriaController.getCategorias)
+router.get("/",categoriaController.getCategorias);
+
+/* configuramos respuesta desde server metodo http post*/
+router.post("/",categoriaController.postCategorias);
+
+/* ruta que recibe un parametro tipo get*/
+router.get("/:id",categoriaController.getCategory);
+
+/* ruta que elimina un solo parametro */
+router.delete("/:id", categoriaController.deleteCategory);
+
+/* ruta que actualiza por parametro*/
+router.put("/:id", categoriaController.updateCategoria);
+
 
 /* hacemos disponible el router en toda la app*/
 export default router;
